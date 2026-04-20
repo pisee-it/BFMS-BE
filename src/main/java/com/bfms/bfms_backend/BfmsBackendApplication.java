4d521bc3f85237e503fbd63f767e845960510eaa
pisee-it <piseeit@gmail.com>
2026-04-20 14:19:50 +0700

    This is the first commit, just created Project
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/bfms/bfms_backend/BfmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bfms.bfms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bfms/bfms_backend/BfmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfms.bfms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
